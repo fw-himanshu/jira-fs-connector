181c732bafb9f4abe1b2e10c85894aa84cb1b0cd
balasenthilduraiswamy <balasenthil.duraiswamy@freshworks.com>
2024-10-24 15:30:11 +0530

    for demo2

---
+++ src/test/java/com/freshworks/ip/external/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshworks.ip.external.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
